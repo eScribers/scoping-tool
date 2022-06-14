7f17c665207e224a56ce2aa0367217f54950cc38
Tom Gannot <Tgannot@escribers.net>
2022-06-14 11:38:49 +0300

    initial commit

---
+++ src/components/audioPlayer/AudioPlayer.tsx
@@ -0,0 +1,17 @@
+import React, { useState, useRef, useEffect, useCallback } from "react";
+interface AudioPlayerProps {
+  src: string;
+  ref: React.RefObject<HTMLAudioElement>;
+  playHead: number;
+}
+export const AudioPlayer = ({
+  src,
+  ref,
+  playHead,
+}: AudioPlayerProps) => {
+
+
+  return (
+    null
+  );
+};
