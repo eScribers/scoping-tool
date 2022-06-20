9d30b2bd6004da608c992e5b64548d8d106f75db
oKovalenkoPolytech <o.kovalenko@neo.polytech.software>
2022-06-20 20:26:10 +0300

    Update transcribeChanges when we add new word
    Fixed issue with word index

--- src/components/ScoopingTool/TranscriptFile/TranscriptFile.tsx
+++ src/components/ScoopingTool/TranscriptFile/TranscriptFile.tsx
@@ -22,16 +22,21 @@ interface TranscriptChangesInterface {
     newWord: String | null
 }
 
+interface triggerElemInterface {
+    prevElem:string,
+    currentElem: string
+}
+
 const textContainerStyle: CSSProperties = {
     flex: 1,
     overflowY: 'scroll'
 }
 
+
 const TranscriptFile: FC<TranscriptFileInterface> = ({playHead}) => {
-    const [transcriptChanges, setTranscriptChanges] = useState<TranscriptChangesInterface[]>([
-    ]);
+    const [transcriptChanges, setTranscriptChanges] = useState<TranscriptChangesInterface[]>([]);
     const [transcriptFile, setTranscriptFile] = useState<SentenceInterface[]>([]);
-    const [triggerElement, setTriggerElement] = useState<string | null>(null)
+    const [triggerElement, setTriggerElement] = useState<triggerElemInterface | null>(null)
     const textContainerRef = useRef<HTMLDivElement>(null)
 
     const loadFile = async () => {
@@ -59,12 +64,14 @@ const TranscriptFile: FC<TranscriptFileInterface> = ({playHead}) => {
     useEffect(() => {
         if (triggerElement !== null) {
             // @ts-ignore
-            document.getElementById(triggerElement).focus()
+            document.getElementById(triggerElement.prevElem).blur()
+            // @ts-ignore
+            document.getElementById(triggerElement.currentElem).focus()
         }
     }, [triggerElement])
 
     useEffect(() => {
-        console.log(transcriptChanges)
+        console.log('transcriptChanges', transcriptChanges)
     }, [transcriptChanges])
 
     const sendChanges = async () => {
@@ -77,16 +84,12 @@ const TranscriptFile: FC<TranscriptFileInterface> = ({playHead}) => {
     }
 
 
-
-
-    
-    const handleTranscriptChanges = (oldword: string, word: string, sIndex: number, wordIndex: number) => { 
+    const handleTranscriptChanges = (oldword: string, word: string, sIndex: number, wordIndex: number) => {
         //if sIndex and sIndex equel to current object.sIndex and object.wIndex in transcriptChanges then update the word:
         const transcriptChange = transcriptChanges.find((change: TranscriptChangesInterface) => (
-            change.sIndex === sIndex && change.wIndex === wordIndex +1
+            change.sIndex === sIndex && change.wIndex === wordIndex
         ))
 
-        console.log(transcriptChange)
         if (transcriptChange) {
             if (word === '') {
                 //if word is null then we edit the action in our transcriptChanges object to remove:
@@ -101,70 +104,81 @@ const TranscriptFile: FC<TranscriptFileInterface> = ({playHead}) => {
                 }))
             } else {
                 setTranscriptChanges(transcriptChanges.map((change: TranscriptChangesInterface) => {
-                    console.log(change)
-                    if (change.sIndex === sIndex && change.wIndex === wordIndex) {
-                        return {
-                            ...change,
-                            newWord: word
+                        if (change.sIndex === sIndex && change.wIndex === wordIndex) {
+                            return {
+                                ...change,
+                                newWord: word
+                            }
                         }
+                        return change
                     }
-                    return change
-                }
                 ))
-            } 
+            }
         } else {
             if (transcriptFile[sIndex].Words[wordIndex].hasOwnProperty('IsNewWord') === false) {
                 setTranscriptChanges([...transcriptChanges, {
-                    action: word === '' ? 'delete': 'replace',
+                    action: word === '' ? 'delete' : 'replace',
                     sIndex,
                     wIndex: wordIndex,
                     word: oldword,
                     newWord: word
                 }])
             } else {
-                
+
                 setTranscriptChanges([...transcriptChanges, {
                     action: 'add',
                     sIndex,
-                    wIndex: wordIndex+1,
+                    wIndex: wordIndex,
                     word: oldword,
                     newWord: word
                 }])
 
+            }
         }
     }
-}
 
-    
-    const onChangeWord = (oldword: string, word: string | null, sIndex: number, wordIndex: number) => {
+
+    const onChangeWord = (oldword: string, word: string | null, sIndex: number, wordIndex: number, keyCode:string) => {
         if (word === null || oldword === word || word === '') {
             return false
         }
 
         const updateTranscriptFile = _.cloneDeep(transcriptFile)
-        updateTranscriptFile[sIndex].Words[wordIndex].Text = word
 
-
-        let spaceSymbol = new RegExp("\\s+");
-        //if last symbol === space, we adding new empty word object and update our transcript file an
-        if (word.split('')[word.length - 1].match(spaceSymbol)) {
+        //if press key === space, we adding new empty word object and update our transcript file an
+        if (keyCode === "Space") {
             let emptyWordObject: WordInterface = {
                 ConfidenceLevel: null,
                 FormatWord: null,
                 LogNote: null,
                 Tag: 0,
-                Text: '',
+                Text: ' ',
                 TimeRange: null,
                 IsNewWord: true
             }
             updateTranscriptFile[sIndex].Words.splice(wordIndex + 1, 0, emptyWordObject)
-            
-            setTranscriptFile(updateTranscriptFile)
+
+
             //we need update transcriptChanges if(wordIndex > object.wIndex):
+            if (transcriptChanges.length > 0) {
+                const newTranscriptChanges = transcriptChanges.map(change => {
+                    if (sIndex === change.sIndex && wordIndex + 1 <= change.wIndex) {
+                        return {
+                            ...change,
+                            wIndex: change.wIndex + 1
+                        }
+                    }
+                    return change
+                })
+                setTranscriptChanges(newTranscriptChanges)
+            }
 
-
+            setTranscriptFile(updateTranscriptFile)
             //Set new triggerElem for change focus
-            setTriggerElement(`sentence_${sIndex}_word_${wordIndex + 1}`)
+            setTriggerElement({
+                prevElem: `sentence_${sIndex}_word_${wordIndex}`,
+                currentElem: `sentence_${sIndex}_word_${wordIndex + 1}`
+            })
         }
 
     }
@@ -174,8 +188,12 @@ const TranscriptFile: FC<TranscriptFileInterface> = ({playHead}) => {
             return false
         }
 
-        console.log(wordIndex);
-        handleTranscriptChanges(oldword, word, sIndex , wordIndex);
+        const updateTranscriptFile = _.cloneDeep(transcriptFile)
+        updateTranscriptFile[sIndex].Words[wordIndex].Text = word
+
+        setTranscriptFile(updateTranscriptFile)
+
+        handleTranscriptChanges(oldword, word, sIndex, wordIndex);
 
     }
 
@@ -223,9 +241,10 @@ const TranscriptFile: FC<TranscriptFileInterface> = ({playHead}) => {
                                                 style={isInTimeRange ? {background: 'yellow'} : {}}
                                                 contentEditable
                                                 suppressContentEditableWarning={true}
-                                                onInput={(e) => {
-                                                    onChangeWord(word.Text, e.currentTarget.textContent, s_index, index)
+                                                onKeyUp={(e) => {
+                                                    onChangeWord(word.Text, e.currentTarget.textContent, s_index, index, e.code)
                                                 }}
+
                                                 onBlur={(e) => {
                                                     onBlurWord(word.Text, e.currentTarget.textContent, s_index, index)
                                                 }}
